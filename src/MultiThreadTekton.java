b098b9e66b1430c3e461bfb9a4c12cb8a4e23f6b
Ji Yuang <969619367woani@gmail.com>
2025-03-21 21:09:51 +0100

    error fixed 1

--- src/MultiThreadTekton.java
+++ src/MultiThreadTekton.java
@@ -1,7 +1,8 @@
 public class MultiThreadTekton extends Tekton{
     @Override
-    public boolean addThread(MushroomBody mb){
+    public boolean addThread(MushroomThread mt){
         System.out.println(">MultiThreadTekton.tektonEffect(): void");
         System.out.println("<");
+        return true;
     }
 }
