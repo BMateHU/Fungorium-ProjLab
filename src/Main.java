786955364ab873f84f8f6b4600c01cc499f9d705
Yin Jun <1171276297yin@gmail.com>
2025-03-22 22:33:02 +0100

    made printing a bit prettier

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,9 @@ public class Main {
         Szkeleton s = new Szkeleton();
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("1. Rovar mozgás effektus nélkül");
-            System.out.println("2. Rovar mozgás lassító effektussal");
-            System.out.println("3. Rovar mozgás gyorsító effektussal");
-            System.out.println("4. Rovar mozgás bénító effektussal");
-            System.out.println("5. Rovar mozgás sikertelen");
-            System.out.println("6. Rovar vágás sikertelen");
-            System.out.println("7. Rovar vágás");
-            System.out.println("8. Gombatest növesztés");
-            System.out.println("9. Gombatest növesztése sikertelen");
-            System.out.println("10. Spóraszórás");
-            System.out.println("11. Spóraszórás sikertelen");
-            System.out.println("12. Gombafonal növesztése");
-            System.out.println("13. Gombafonal növesztés sikertelen");
-            System.out.println("14. Gombafonal gyorsítás");
-            System.out.println("15. Tekton kettétörése");
-            System.out.println("16. Tekton kettétörése sikertelen");
-            System.out.println("17. Rovar Eszik (gyorsító Spóra)");
-            System.out.println("18. Rovar Eszik (lassító Spóra)");
-            System.out.println("19. Rovar Eszik (bénító Spóra)");
-            System.out.println("20. Rovar Eszik (némító Spóra)");
-            System.out.println("21. Rovar Eszik");
-            System.out.println("0. Exit");
-            System.out.println("Please enter the use case number you wanna test:");
-
+            printMenu();
             int useCase = scanner.nextInt();
+            System.out.println("--------------------------------------------------------");
             switch (useCase) {
                 case 1:
                     s.useCase1();
@@ -100,6 +78,34 @@ public class Main {
                 default:
                     exit(0);
             }
+            System.out.println("--------------------------------------------------------");
         } while(true);
+    }
+
+    public static void printMenu()
+    {
+        System.out.println("1. Rovar mozgás effektus nélkül");
+        System.out.println("2. Rovar mozgás lassító effektussal");
+        System.out.println("3. Rovar mozgás gyorsító effektussal");
+        System.out.println("4. Rovar mozgás bénító effektussal");
+        System.out.println("5. Rovar mozgás sikertelen");
+        System.out.println("6. Rovar vágás sikertelen");
+        System.out.println("7. Rovar vágás");
+        System.out.println("8. Gombatest növesztés");
+        System.out.println("9. Gombatest növesztése sikertelen");
+        System.out.println("10. Spóraszórás");
+        System.out.println("11. Spóraszórás sikertelen");
+        System.out.println("12. Gombafonal növesztése");
+        System.out.println("13. Gombafonal növesztés sikertelen");
+        System.out.println("14. Gombafonal gyorsítás");
+        System.out.println("15. Tekton kettétörése");
+        System.out.println("16. Tekton kettétörése sikertelen");
+        System.out.println("17. Rovar Eszik (gyorsító Spóra)");
+        System.out.println("18. Rovar Eszik (lassító Spóra)");
+        System.out.println("19. Rovar Eszik (bénító Spóra)");
+        System.out.println("20. Rovar Eszik (némító Spóra)");
+        System.out.println("21. Rovar Eszik");
+        System.out.println("0. Exit");
+        System.out.println("Please enter the use case number you wanna test:");
     }
 }
