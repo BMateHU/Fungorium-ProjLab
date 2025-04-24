a72acbb3a61d9db8d0d4fb47274a4e429b5a23f6
BMate <113548528+BMateHU@users.noreply.github.com>
2025-04-24 22:16:23 +0200

    Tests written

--- src/main/java/com/beingchilling/game/GameModel.java
+++ src/main/java/com/beingchilling/game/GameModel.java
@@ -2,10 +2,7 @@ package com.beingchilling.game;
 
 import com.beingchilling.model.InsectSpecies;
 import com.beingchilling.model.MushroomSpecies;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 
 public class GameModel {
@@ -16,6 +13,7 @@ public class GameModel {
     public static Map map = new Map();
     public static HashMap<String, MushroomSpecies> gombasz = new HashMap<>();
     public static HashMap<String, InsectSpecies> rovarasz = new HashMap<>();
+
     public static BiMap<String, Object> gameObjects = new BiMap<>();
 
 }
