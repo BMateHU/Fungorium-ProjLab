ff95610e86f6914714ad3d7b815582ad9d6c44bc
Yin Jun <1171276297yin@gmail.com>
2025-04-25 20:17:06 +0200

    added some comment and rewrite some functions

--- src/main/java/com/beingchilling/game/BiMap.java
+++ src/main/java/com/beingchilling/game/BiMap.java
@@ -11,69 +11,91 @@ public class BiMap<K, V> {
     private final HashMap<K, V> forwardMap = new HashMap<>();
     private final HashMap<V, K> backwardMap = new HashMap<>();
 
+    /**
+     * Associates the specified value with the specified key in this map.
+     * @param key the key
+     * @param value the value
+     */
     public void put(K key, V value)
     {
         forwardMap.put(key, value);
         backwardMap.put(value, key);
     }
 
+    /**
+     * Associates the specified value with the specified key in this map.
+     * @param value the value
+     * @return the key
+     */
     public K getK(V value)
     {
         return backwardMap.get(value);
     }
 
+    /**
+     * Returns the value to which the specified key is mapped
+     * @param key the value
+     * @return the value
+     */
     public V getV(K key)
     {
         return forwardMap.get(key);
     }
 
+    /**
+     * Remove the pair from the map by key
+     * @param key the value
+     */
     public void removeByK(K key)
     {
         backwardMap.remove(forwardMap.get(key));
         forwardMap.remove(key);
     }
 
+    /**
+     * Remove the pair from the map by value
+     * @param value the value
+     */
     public void removeByV(V value)
     {
         forwardMap.remove(backwardMap.get(value));
         backwardMap.remove(value);
     }
 
-    public void getKOrDefault(V value)
-    {
-        backwardMap.getOrDefault(value,null);
-    }
-
-    public void getVOrDefault(K key)
-    {
-        forwardMap.getOrDefault(key,null);
-    }
-
-    public void clear()
-    {
-        forwardMap.clear();
-        backwardMap.clear();
-    }
-
+    /**
+     * return all the keys in the map
+     */
     public Set<K> keySet()
     {
         return forwardMap.keySet();
     }
 
+    /**
+     * return all the values in the map
+     */
     public Set<V> valueSet()
     {
         return backwardMap.keySet();
     }
 
+    /**
+     * check if the map contains the Key
+     */
     public boolean containsK(K key)
     {
         return forwardMap.containsKey(key);
     }
 
+    /**
+     * check if the map contains the Value
+     */
     public boolean containsV(V value)
     {
         return backwardMap.containsKey(value);
     }
 
+    /**
+     * the size of the map
+     */
     public int size(){return backwardMap.size();}
 }
