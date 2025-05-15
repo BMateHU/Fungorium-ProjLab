8545bfd6204a7265072a0eadfa84223588b05a19
BMate <113548528+BMateHU@users.noreply.github.com>
2025-05-15 20:44:09 +0200

    fix, initial classes

--- src/main/java/com/beingchilling/Main.java
+++ src/main/java/com/beingchilling/Main.java
@@ -1,33 +1,11 @@
 package com.beingchilling;
 
-import com.beingchilling.controller.ControllerComponent;
-import com.beingchilling.view.GUI;
-import com.beingchilling.view.ViewComponent;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributeView;
-import java.nio.file.attribute.BasicFileAttributes;
+import com.beingchilling.gui.GUI;
 
 public class Main {
 
-    public static ViewComponent vc = new ViewComponent();
-    public static ControllerComponent cc = new ControllerComponent(vc);
-
-    public static void main(String[] args) throws IOException {
-//        vc.setControllerComponent(cc);
-//        try {
-//            InputStream url = Main.class.getClassLoader().getResourceAsStream("start.txt");
-//            assert url != null;
-//            cc.load(url);
-//        } catch (RuntimeException e) {
-//            throw new RuntimeException(e);
-//        }
-//        cc.gameLoop();
+    public static void main(String[] args) {
         GUI frame = new GUI();
-        frame.setVisible(true);
         frame.switchPanels();
     }
 }
