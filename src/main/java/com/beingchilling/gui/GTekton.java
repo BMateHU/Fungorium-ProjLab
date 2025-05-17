ce57b2325913272250a697a7aed2df8bce338e2c
benem <matthew.be04@gmail.com>
2025-05-17 11:35:32 +0200

    paintcomponent finished?

--- src/main/java/com/beingchilling/gui/GTekton.java
+++ src/main/java/com/beingchilling/gui/GTekton.java
@@ -3,17 +3,39 @@ package com.beingchilling.gui;
 import com.beingchilling.view.TektonView;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class GTekton extends JComponent {
 
     TektonView tekton;
 
-    private int x, y;
+    ArrayList<JComponent> spores = new ArrayList<>();
+    JComponent mushroomBody;
+    JComponent insect;
+
+    private int x, y; //this should mark center point of the circle, (as I saw the normal X and Y is the left upper corner of the circle (so just - radius)
 
     public GTekton(TektonView tekton) {
         this.tekton = tekton;
     }
 
+    @Override
+    public void paint(Graphics g) {
+        tekton.getSpores().forEach(spore -> spores.add(GUI.objects.getV(spore)));
+        mushroomBody = GUI.objects.getV(tekton.getBody());
+        insect = GUI.objects.getV(tekton.getInsect());
+
+        super.paint(g);
+
+        mushroomBody.paint(g);
+        insect.paint(g);
+        for(int i = 0; i < 4; i++) {
+            spores.get(i).paint(g);
+        }
+    }
+
+    @Override
     public int getX() {
         return x;
     }
@@ -22,6 +44,7 @@ public class GTekton extends JComponent {
         this.x = x;
     }
 
+    @Override
     public int getY() {
         return y;
     }
