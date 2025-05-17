54be7f2d25a48a5147ef27ed63bda16bfc422291
benem <matthew.be04@gmail.com>
2025-05-17 22:24:48 +0200

    DrawPanel finished

--- src/main/java/com/beingchilling/controller/IFactory.java
+++ src/main/java/com/beingchilling/controller/IFactory.java
@@ -1,11 +1,11 @@
 package com.beingchilling.controller;
 
 import com.beingchilling.model.*;
+import com.beingchilling.view.TektonView;
 
 public interface IFactory {
     void onCreationTekton(Tekton t);
     void onCreationMushroomBody(MushroomBody mb);
-    void onCreationSpore(Spore sp);
+    void onCreationSpore(Spore sp, TektonView t);
     void onCreationInsect(Insect ins);
-    void onCreationMushroomThread(MushroomThread mt);
 }
