4820248206c8016a6a2e5710d6acf32a91355ba5
Ji Jin Yan <fengxinzeisao@gmail.com>
2025-05-17 22:29:24 +0200

    paint implemented

--- src/main/java/com/beingchilling/gui/GInsect.java
+++ src/main/java/com/beingchilling/gui/GInsect.java
@@ -1,14 +1,35 @@
 package com.beingchilling.gui;
 
+import com.beingchilling.game.GameModel;
+import com.beingchilling.model.Tekton;
 import com.beingchilling.view.InsectView;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Path2D;
 
 public class GInsect extends JComponent {
 
     InsectView insect;
-
     public GInsect(InsectView insect) {
         this.insect = insect;
     }
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        super.paint(g);
+        int coordX = GUI.objects.getV(insect.getLocation()).getX();
+        int coordY = GUI.objects.getV(insect.getLocation()).getY();
+        int squareSize = 20;
+        g2d.setColor(new Color(139, 69, 19));
+        g2d.fillRect(coordX, coordY, squareSize, squareSize);
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(coordX, coordY, squareSize, squareSize);
+        FontMetrics fm = g2d.getFontMetrics();
+        String insectId = GameModel.gameObjects.getK(this);
+        //this add the Id next to the object
+        g2d.drawString(insectId, coordX + 10, coordY);
+
+    }
+
 }
--- src/main/java/com/beingchilling/gui/GTekton.java
+++ src/main/java/com/beingchilling/gui/GTekton.java
@@ -1,5 +1,6 @@
 package com.beingchilling.gui;
 
+import com.beingchilling.game.GameModel;
 import com.beingchilling.view.TektonView;
 
 import javax.swing.*;
@@ -7,13 +8,11 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class GTekton extends JComponent {
-
     TektonView tekton;
-
     ArrayList<JComponent> spores = new ArrayList<>();
     JComponent mushroomBody;
     JComponent insect;
-
+    public static final int RADIUS = 20;
     private int x, y; //this should mark center point of the circle, (as I saw the normal X and Y is the left upper corner of the circle (so just - radius)
 
     public GTekton(TektonView tekton) {
@@ -22,17 +21,32 @@ public class GTekton extends JComponent {
 
     @Override
     public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
         tekton.getSpores().forEach(spore -> spores.add(GUI.objects.getV(spore)));
         mushroomBody = GUI.objects.getV(tekton.getBody());
         insect = GUI.objects.getV(tekton.getInsect());
-
         super.paint(g);
+        //kor
+        g2d.setColor(Color.BLUE);
+        g2d.setStroke(new BasicStroke(2));
+        g2d.drawOval(x, y, RADIUS * 2, RADIUS * 2);
+        FontMetrics fm = g2d.getFontMetrics();
+        String tektonId = GameModel.gameObjects.getK(this);
+        g2d.drawString(tektonId, x, y + RADIUS + 20);
 
         mushroomBody.paint(g);
         insect.paint(g);
-        for(int i = 0; i < 4; i++) {
-            spores.get(i).paint(g);
-        }
+        spores.getFirst().paint(g);
+
+
+
+        //spora szam kiiras
+
+        //id kiiras tekton ala
+        //FontMetrics fm = g2d.getFontMetrics();
+        // String threadLabel = GameModel.gameObjects.getK(t2.getThreads().get(0));
+        // int tekton1Width = fm.stringWidth(threadLabel);
+        // g2d.drawString(threadLabel, x+(x-x2) - tekton1Width/2, y+(y-y2));
     }
 
     @Override
